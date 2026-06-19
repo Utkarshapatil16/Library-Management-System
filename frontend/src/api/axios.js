1af7cdcf8360062f99acaa360aa27e8224066485
Utkarsha <utkarshapatil510@gmail.com>
2026-06-19 16:58:50 +0530

    Update backend API URL

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Base URL for Django backend
 const API = axios.create({
-    baseURL:'https://library-management-system-v2y6.vercel.app/api',,
+    baseURL: 'https://library-management-system-v2y6.vercel.app/api',
 });
 
 // Automatically add JWT token to every request
@@ -24,7 +24,7 @@ API.interceptors.response.use(
             try {
                 const refresh = localStorage.getItem('refresh');
                 const res = await axios.post(
-                    'https://library-management-system-v2y6.vercel.app/api/students/token/refresh/',,
+                    'https://library-management-system-v2y6.vercel.app/api/students/token/refresh/',
                     { refresh }
                 );
                 localStorage.setItem('access', res.data.access);
